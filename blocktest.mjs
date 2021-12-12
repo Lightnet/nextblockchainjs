35d71dca9094f74a63405e46190cc2efaefe5a04
Lightnet <darkneter@gmail.com>
2021-12-11 21:47:58 -0800

    tested block hash prevhash

--- blocktest.mjs
+++ blocktest.mjs
@@ -1,11 +1,49 @@
 
+
+
+
+
+
 //import SHA256 from 'crypto-js/sha256.js';
 import CryptoJS from 'crypto-js';
 import { BlockChain } from './lib/blockchain.mjs';
+import clientDB from './lib/database.mjs';
+import {proofOfWork} from './lib/validator.mjs';
 
-let blockChain = new BlockChain();
+let PROOF = 1560;
+
+let hash = (data)=>{
+  return String(CryptoJS.MD5(String(data)));
+}
+
+async function main(){
+  let db = await clientDB();
+
+  console.log("init block")
+  let blockChain = new BlockChain();
+
+  console.log("check...")
+  console.log(proofOfWork())
+  console.log(hash(PROOF))
+  console.log("hash...")
+  if(proofOfWork() == hash(PROOF)){
+    console.log("PASS!!!")
+    blockChain.addNewTransaction('islem','alex',200);
+    let prevHash = blockChain.lastBlock() ? blockChain.lastBlock().hash : null;
+    await blockChain.addNewBlock(prevHash);
+
+    console.log('chain: ',blockChain.chain);
+  }else{
+    console.log("fail check")
+  }
+}
+
+main();
 
 
+
+
+/*
 let PROOF = 156;
 //let hash = SHA256;
 let hash = (data)=>{
@@ -32,10 +70,10 @@ let proofOfWork = () =>{
   }
   return proof;
 }
-
+*/
 //console.log("String() => ", String(CryptoJS.MD5('hello')));
 //console.log("String() => ", String(CryptoJS.MD5('hello')));
-
+/*
 if(proofOfWork() == PROOF){
   blockChain.addNewTransaction('islem','alex',200);
   let prevHash = blockChain.lastBlock() ? blockChain.lastBlock().hash : null;
@@ -43,3 +81,4 @@ if(proofOfWork() == PROOF){
 }
 
 console.log('chain: ',blockChain.chain);
+*/
